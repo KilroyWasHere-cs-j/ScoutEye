d565a510dd9a3e3018991e2b9fa88c86fc88fda3
gmtow <gmtow@DESKTOP-F0HHK8S>
2022-05-07 14:37:33 -0400

    dc

---
+++ ScoutEye/ScoutEye/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScoutEye
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Pro pro;
+        Am am;
+        public MainWindow()
+        {
+            InitializeComponent();
+            pro = new Pro();
+            am = new Am();
+        }
+
+        private void ScoutOption2_Click(object sender, RoutedEventArgs e)
+        {
+            //Open the professional window and closes the current window
+            if(NameTB.Text != "Name")
+            {
+                pro.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please enter your name.", "Missing fields", MessageBoxButton.OK, MessageBoxImage.Hand);
+            }
+        }
+
+        private void ScoutOption1_Click(object sender, RoutedEventArgs e)
+        {
+            //Open the amateur window and closes the current window
+            if (NameTB.Text != "Name")
+            {
+                am.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please enter your name.", "Missing fields", MessageBoxButton.OK, MessageBoxImage.Hand);
+            }
+        }
+    }
+}
---
+++ ScoutEye/ScoutEye/Pro.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Xml;
+
+namespace ScoutEye
+{
+    /// <summary>
+    /// Interaction logic for Pro.xaml
+    /// </summary>
+    public partial class Pro : Window
+    {
+        private XmlDocument xml;
+        public Pro()
+        {
+            InitializeComponent();
+            xml = new XmlDocument();
+            LoadUIFromSettings();
+        }
+
+        //<summary>
+        //Convert bitmap image to a image source so that the image box will show it
+        //<summary>
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        //<summary>
+        //Load app settings from a xml file
+        //<summary>
+        private void LoadUIFromSettings()
+        {
+            try
+            {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                xml.Load("Settings.xml");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                XmlNodeList nodeList = xml.DocumentElement.SelectNodes("/settings");
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+                foreach (XmlNode node in nodeList)
+                {
+                    VersionLB.Content = node.SelectSingleNode("AppVersion").InnerText;
+                    Auto0LB.Content = node.SelectSingleNode("Auto0").InnerText;
+                    Auto1LB.Content = node.SelectSingleNode("Auto1").InnerText;
+                    Auto2LB.Content = node.SelectSingleNode("Auto2").InnerText;
+                    Teleop0LB.Content = node.SelectSingleNode("Teleop0").InnerText;
+                    Teleop1LB.Content = node.SelectSingleNode("Teleop1").InnerText;
+                    Teleop2LB.Content = node.SelectSingleNode("Teleop2").InnerText;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                }
+                //logger.Log("App loaded from settings");
+            }
+            catch (Exception ex)
+            {
+               // logger.Log("Could not load settings. " + ex.Message);
+                MessageBox.Show("Could not load settings. Failed with exception. " + ex.Message, "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Environment.Exit(0);
+            }
+        }
+
+        #region EventHandlers
+        private void EnterBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}

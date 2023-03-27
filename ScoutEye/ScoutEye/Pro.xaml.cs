dc345dfa66ae4b8544c6d47fdae27eafe47a8efd
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2023-03-26 21:14:12 -0400

    Fixed version number your welcome Baxter

--- ScoutEye/ScoutEye/Pro.xaml.cs
+++ ScoutEye/ScoutEye/Pro.xaml.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Media;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
